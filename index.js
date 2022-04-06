1bca1b6fe041ffc9b1284a6ece166470fe06d7e9
Bishnu <bishnuthapako@gmail.com>
2022-04-06 09:14:10 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+
+function superbowlWin(record){
+    let superBow = record.find(val => val.result === "W")
+    if(superBow){
+        return superBow.year;
+    } else{
+        return undefined
+    }
+}
+superbowlWin()
